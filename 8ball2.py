117cb4ada5795f3af1d36a01844d51f0075633a4
tewlwolow <61018886+tewlwolow@users.noreply.github.com>
2020-02-16 23:27:16 +0100

    cleanup, orthography, function, loop

--- 8ball2.py
+++ 8ball2.py
@@ -1,16 +1,21 @@
 import random
 
-messages = ['It is certain',
-    'It is decidedly so',
-    'Yes definitely',
-    'Reply hazy try again',
-    'Ask again later',
-    'Concentrate and ask again',
-    'My reply is no',
-    'Outlook not so good',
-    'Very doubtful']
+def dice():
+    while True:
+        messages = ["It is certain",
+            "It is decidedly so",
+            "Yes definitely",
+            "Reply hazy try again",
+            "Ask again later",
+            "Concentrate and ask again",
+            "My reply is no",
+            "Outlook not so good",
+            "Very doubtful"]
+        ans=input("Think of something you want to know. Then type 'throw' to begin. Enter anything else to shoo me away.")
+        if ans=="throw":
+            print(messages[random.randint(0,len(messages)-1)])
+        else:
+            print("Begone!")
+            quit()
 
-for i in messages:
-    print(messages[random.randint(0, len(messages) - 1)])
-    continue
-    
+dice()
