86a8262fb5bc6a33529ecfcd886d19de508a4e7f
tewlwolow <61018886+tewlwolow@users.noreply.github.com>
2020-02-16 23:30:20 +0100

    cleanup, loop, try/except

--- age1.py
+++ age1.py
@@ -1,10 +1,13 @@
-print('Hello!')
-print('Whats your name?')       #ask for their name
-myName = input()
-print('It is good to meet you, ' + myName)
-print('The length of your name is:')
-print(len(myName))
-print('What is your age?')      #ask for their age
-myAge = input()
-print('You will be ' + str(int(myAge) +1) + ' in a year.')
-
+while True:
+    try:
+        print("Hello!")
+        print("Whats your name?")
+        myName=input()
+        print("It is good to meet you,",myName)
+        print("The length of your name is:")
+        print(len(myName))
+        print("What is your age?")
+        myAge=input()
+        print("You will be",str(int(myAge) +1),"in a year.")
+    except:
+        print("Weird. Try again.")
