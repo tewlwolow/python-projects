9559d9a20cd166bd334cd59c11408c7460be9e32
tewlwolow <61018886+tewlwolow@users.noreply.github.com>
2020-02-16 23:02:50 +0100

    Delete elen00tka.py

--- elen00tka.py
+++
@@ -1,8 +0,0 @@
-while True:
-    print("Hej. Kim jesteś?")
-    name=input()
-    if name!="Elena":
-        continue
-    if name=="Elena":
-        break
-print("Bardzo kocham moją Elenutkę!!! <3")
