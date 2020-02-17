e71606b51c04e414a62c931676b29f4bc8affd41
tewlwolow <61018886+tewlwolow@users.noreply.github.com>
2020-02-17 01:18:57 +0100

    added early results check

--- ex8-RPS v1.1.py
+++ ex8-RPS v1.1.py
@@ -1,7 +1,6 @@
 # import sys module for final smooth exit function
 import sys
 
-
 # quick match game mode
 def quick():
     # general loop for quick match game mode
@@ -63,12 +62,14 @@ def normal():
         print("The programme will finish now.")
         quit()
     usr_round_nmb=int(usr_round_nmb)
+
     # define any round in normal game mode
     def round():
         # general loop in normal game mode round
         rc=1 # round counter
         plonesc=0 # player one score
         pltwosc=0 # player one score
+        print("If you want to finish and get results before completing all rounds, type 'Get results'.")
         while True:
             # player one loop + error catching
             if rc>usr_round_nmb:
@@ -77,19 +78,26 @@ def normal():
                 while True:
                     print("Round",rc) # print round number
                     player_one=input("PLAYER ONE: play rock, paper, or scissors: ")
+                    if player_one=="Get results" or player_one=="get results":
+                        break
                     if player_one=="leave": # forcequit in normal game mode player one input
                         print("The programme will finish now.")
                         quit()
-                    elif player_one!="rock" and player_one!="scissors" and player_one!="paper":
+                    if player_one!="rock" and player_one!="scissors" and player_one!="paper":
                         print("Oops! Try again!")
                         continue
                     else:
                         break
-
+                    
+                if player_one=="Get results" or player_one=="get results":
+                    break
+                
                 # player two loop + error catching       
                 while True:
                     player_two=input("PLAYER TWO: play rock, paper, or scissors: ")
-                    if player_two=="leave": # forcequit in quick match mode player one input
+                    if player_two=="Get results" or player_two=="get results":
+                        break
+                    elif player_two=="leave": # forcequit in quick match mode player one input
                         print("The programme will finish now.")
                         quit()
                     elif player_two!="rock" and player_two!="scissors" and player_two!="paper":
@@ -99,6 +107,8 @@ def normal():
                         break
                     
                 # general loop scoring conditions + messages
+                if rc>usr_round_nmb or player_one=="Get results" or player_one=="get results" or player_two=="Get results" or player_two=="get results":
+                    break
                 if player_one=="rock"and player_two=="paper":
                     print("Paper beats rock! Player two scores.")
                     pltwosc+=1 # player one scores round etc.
@@ -127,10 +137,12 @@ def normal():
                     print ("It's a draw! Nobody scores.")
                     break
                 return plonesc, pltwosc
+                
+                
             rc+=1 # increments round number
 
             # evaluate scores after all rounds
-            if rc>usr_round_nmb:
+            if rc>usr_round_nmb or player_one=="Get results" or player_one=="get results" or player_two=="Get results" or player_two=="get results":
                 if plonesc>pltwosc:
                     print("Congratulations! Player one won!")
                 elif pltwosc>plonesc:
