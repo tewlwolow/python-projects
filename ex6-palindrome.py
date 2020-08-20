b4b59641942e279e1bcdb3741d323b57c06a0df2
tewlwolow <61018886+tewlwolow@users.noreply.github.com>
2020-08-20 14:39:22 +0200

    Delete ex6-palindrome.py

--- ex6-palindrome.py
+++
@@ -1,8 +0,0 @@
-def palin():
-    pal=input("Please write any word: ").lower()
-    if pal[::-1]==pal:
-        print("It's a palindrome.")
-    else:
-        print ("Whatever.")
-        
-palin()
