48f41d9528a72796bfe4fd847564a1fe7a751236
tewlwolow <61018886+tewlwolow@users.noreply.github.com>
2020-09-23 00:09:17 +0200

    Create Ultimate Palindrome Checker.py
    simple palindrome shtuff

---
+++ Ultimate Palindrome Checker.py
@@ -0,0 +1,12 @@
+import sys
+
+print("Welcome to the Ultimate Palindrome Checker v 1.0.0. Type 'EXIT' to exit.")
+
+while True:
+    var = str(input("Please provide any word. Case doesn't matter.: "))
+    if var == "EXIT":
+        sys.exit()
+    elif var[::-1] == var or var[::-1].lower() == var.lower():
+        print("The word provided is a palindrome.")
+    else:
+        print("The word provided is not a palindrome.")     
