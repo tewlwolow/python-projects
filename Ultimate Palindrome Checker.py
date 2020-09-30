1a9f6a0667f2bb6733124b1fc5f82cd3c0838bf5
tewlwolow <leonczernecki@gmail.com>
2020-09-30 15:45:45 +0200

    clean-up

--- Ultimate Palindrome Checker.py
+++ Ultimate Palindrome Checker.py
@@ -3,10 +3,10 @@ import sys
 print("Welcome to the Ultimate Palindrome Checker v 1.0.0. Type 'EXIT' to exit.")
 
 while True:
-    var = str(input("Please provide any word. Case doesn't matter.: "))
+    var = str(input("Please provide any word. Case doesn't matter: "))
     if var == "EXIT":
         sys.exit()
-    elif var[::-1] == var or var[::-1].lower() == var.lower():
+    elif var[::-1].lower() == var.lower():
         print("The word provided is a palindrome.")
     else:
-        print("The word provided is not a palindrome.")     
+        print("The word provided is not a palindrome.")
