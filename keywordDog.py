b475ffe89b75c9c3d6ec834733fd7e3a1cd2be8c
tewlwolow <61018886+tewlwolow@users.noreply.github.com>
2021-09-01 12:19:35 +0200

    Create keywordDog.py

---
+++ keywordDog.py
@@ -0,0 +1,23 @@
+# Just putting this little thing here for future reference.
+# Dogs take keyword arguments too, it turns out. What do you know?
+
+class Dog():
+    def __init__(self, **kwargs):
+        self.name = kwargs["name"]
+        self.age = kwargs["age"]
+        self.attitude = kwargs["attitude"]
+        
+    def showName(self):
+        print("Dog's name is " + self.name + ".")
+        
+    def showAge(self):
+        print("Dog's age is " + str(self.age) + ".")
+        
+    def showAttitude(self):
+        print("Dog's attitude is " + self.attitude + ".")
+
+# Testing stuff. He's been eating way too much recently. Not my fault though.        
+bunio = Dog(name="Bunio", age = 3, attitude = "hongry")
+bunio.showName()
+bunio.showAge()
+bunio.showAttitude()
